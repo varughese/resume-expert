4cb92687138d16598dde353e6608e50af6a5c49d
Mathew Varughese <mat.varughese3@gmail.com>
2020-08-28 17:35:35 -0400

    homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,11 +15,13 @@ const toDate = (date) => {
 
 const ResumeListing = ({ resumes, deleteResume }) => {
     const resumeIds = Object.keys(resumes);
+    let validResumes = 0;
 
     const values = resumeIds.reverse().map((id) => {
         if (!resumes[id]) return null;
         const date = toDate(resumes[id]);
         if (!date) return null;
+        validResumes++;
         return (
             <tr key={id}>
                 <td>{date.toDateString()}</td>
@@ -37,7 +39,7 @@ const ResumeListing = ({ resumes, deleteResume }) => {
             </tr>
         );
     });
-    return values.length ? (
+    return validResumes > 0 ? (
         <table className="listing">
             <thead>
                 <tr>
@@ -49,7 +51,7 @@ const ResumeListing = ({ resumes, deleteResume }) => {
             <tbody>{values}</tbody>
         </table>
     ) : (
-        <p>Loading ... </p>
+        <p>No resumes, upload a new one by clicking below!</p>
     );
 };
 
@@ -57,6 +59,7 @@ const HomeBase = ({ firebase }) => {
     const [user, setUser] = useState("");
     const [uploading, setUploading] = useState("NOT_UPLOADED");
     const [resumes, setResumes] = useState({});
+    const [loading, setLoading] = useState(true);
     const history = useHistory();
 
     const onDrop = useCallback(
@@ -74,10 +77,12 @@ const HomeBase = ({ firebase }) => {
     const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
     const getUser = async () => {
+        setLoading(true);
         const userData = (await firebase.user_ref().get()).data();
         setUser(userData);
         const oldResumes = { ...resumes, ...userData.resumes };
         setResumes(oldResumes);
+        setLoading(false);
     };
 
     const signOut = async () => {
@@ -98,18 +103,24 @@ const HomeBase = ({ firebase }) => {
                 Sign out
             </button>
             <div className="box mt">
-                <p>
-                    Here are a list of your uploaded resumes. To send to
-                    reviewers, click "View Comments" and send the link to who
-                    you want to review it.
-                </p>
-                <ResumeListing
-                    resumes={resumes}
-                    deleteResume={(id) => {
-                        setResumes({ ...resumes, ...{ [id]: false } });
-                        firebase.deleteResume(id);
-                    }}
-                />
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div>
+                        <p>
+                            Here are a list of your uploaded resumes. To send to
+                            reviewers, click "View Comments" and send the link
+                            to who you want to review it.
+                        </p>
+                        <ResumeListing
+                            resumes={resumes}
+                            deleteResume={(id) => {
+                                setResumes({ ...resumes, ...{ [id]: false } });
+                                firebase.deleteResume(id);
+                            }}
+                        />
+                    </div>
+                )}
             </div>
             <div className="gray">
                 {uploading === "NOT_UPLOADED" && (
